84f97f1cac1c386bc0467972a1e66583446a0055
goranti-maneesh <gorantimaneeshreddy@gmail.com>
2024-09-24 23:55:17 +0530

    commit messgae

--- server.js
+++ server.js
@@ -1,5 +1,5 @@
 const express = require("express");
-const connectDB = require("../config/db");
+const connectDB = require("./src/config/db");
 const cors = require("cors");
 
 const app = express();
@@ -9,9 +9,9 @@ console.log(connectDB);
 app.use(express.json());
 app.use(cors());
 
-app.use("/api", require("../routes/booking"));
-app.use("/api", require("../routes/auth"));
-app.use("/api", require("../routes/addData"))
+app.use("/api", require("./src/routes/booking"));
+app.use("/api", require("./src/routes/auth"));
+app.use("/api", require("./src/routes/addData"))
 
 const PORT = 3001;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
